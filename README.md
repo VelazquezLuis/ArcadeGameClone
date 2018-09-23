066d35e1ce6358c1f5519817ac9de4abec0abe62
VelazquezLuis <javiervelazquez113@yahoo.com>
2018-09-23 10:53:24 -0700

    created instances of hero and enemies with thier interactions

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 frontend-nanodegree-arcade-game
 ===============================
-
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
-
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
-# ArcadeGameClone
+### Udacity Front-end Web-development Project 3 
+## Objective of game
+You are a Hero trying to make your way across the road evading enemies at all cost. Do not let any enemies touch you. 
+## instructions 
+using your keyboard arrows move your hero to the last road. contact with an enemy will restart the game. To play, click on the link: good luck!
+## Contributing
+Most of the code and start up files was provided by Udacity. most work was done in the app.js  with the help of walk throughs webinars and udacity resources, this game was able to function properly. 
+## Dependencies 
+ this web app game was built using HTML Canvas, CSS, and Echmascript 6.
--- js/app.js
+++ js/app.js
@@ -1,46 +1,123 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+// GETS THE ELEMENT SPAN
+const gameScore = document.getElementById('gameScore');
+//SETS THE INITIAL TEXT CONTENT OF SPAN TO 0
+let score = gameScore.innerHTML = 0;
+
+// Enemies our player must avoid
+//WE GIVE OUR enemy 3 prams.
+let Enemy = function(x, y, speed) {
+  this.x = x;
+  this.y = y + 55;
+  this.sprite = 'images/enemy-bug.png';
+  this.slide = 101;
+  this.offScreen = this.slide * 5;
+  this.speed = speed;
 };
 
-// Update the enemy's position, required method for game
+// Update the enemy's position
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  if(this.x < this.offScreen) {
+    this.x += this.speed * dt;
+  } else {
+    this.x = -100;
+  }
 };
 
-// Draw the enemy on the screen, required method for game
+// Draws the enemy on the screen
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
+//makes our player model
+class Hero {
+  constructor() {
+    this.sprite = 'images/char-boy.png';
+    this.slide = 101;
+    this.jump = 83;
+    this.startX = this.slide * 2;
+    this.startY = (this.jump * 4) + 55;
+    this.x = this.startX;
+    this.y = this.startY;
+    this.point = 0;
+  }
+  //renders hero in canavas
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+  //keyboard input for hero
+  handleInput(input){
+    switch(input){
+      case 'left':
+      if (this.x >= this.slide){
+        this.x -= this.slide;
+      }
+      break;
+      case 'right':
+      if (this.x <= this.slide * 3){
+        this.x += this.slide;
+      }
+      break;
+      case 'up':
+      if (this.y >= 0) {
+        this.y -= this.jump;
+      }
+      break;
+      case 'down':
+      if (this.y <= this.jump * 4){
+        this.y += this.jump;
+      }
+      break;
+    }
+  }
+  // checks if the enemys collides with hero and manages points.
+  update() {
+    for( let enemy of allEnemies) {
+      if (this.y === enemy.y && (enemy.x + enemy.slide/2 > this.x
+        && enemy.x < this.x + this.slide/2) ){
+        this.reset();
+        if (this.point > 0){
+        this.point --;
+        gameScore.innerHTML = this.point;
+        }
+      }
+    }
+    // once hero cross 3 times alerts a win.
+    if(this.y  === -28 && this.point < 3 ){
+      this.point ++;
+      gameScore.innerHTML = this.point;
+      if(this.point === 3 ){
+        alert("you Win!!")
+      }
+      this.reset();
+    }
+  }
+  // reset function to palce hero at starting position.
+  reset(){
+    this.x = this.startX;
+    this.y = this.startY;
+  }
+}
+// creates new instances of hero and enemies.
+const player = new Hero();
+const bug1 = new Enemy(-100,0, 120);
+const bug2 = new Enemy(-100,83,300);
+const bug3 = new Enemy(-250,83,100);
+const bug4 = new Enemy(-100,166,200);
+const allEnemies = [];
+allEnemies.push(bug1,bug2,bug3,bug4);
+console.log(allEnemies);
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+      37: 'left',
+      38: 'up',
+      39: 'right',
+      40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
